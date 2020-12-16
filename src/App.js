68bb246c92bfb4e923e7239a91c5f54bf84429ff
Andrey Kharebov <andreykharebov@gmail.com>
2020-12-16 15:48:29 +0300

    Added main section

--- src/App.js
+++ src/App.js
@@ -2,10 +2,30 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 
+// Images 
+import mainImage from './assets/home-title.svg';
+import albumImage from './assets/album.jpg';
+
 function App() {
   return (
     <div className='wrapper'>
       <Header />
+      <section className='mainSection'>
+        <div className='content'>
+          <p>new album available</p>
+          <div className='mainImage'>
+            <img src={ mainImage } alt='mainImage'></img>
+          </div>
+          <a href="https://lnk.to/Chronologic" target="_blank" rel="noreferrer">
+            <button className='orderButton'>
+              Order now
+            </button>
+          </a>
+        </div>
+        <div className='picture'>
+          <img src={ albumImage } alt='albumImage'></img>
+        </div>
+      </section>
     </div>
   );
 }
