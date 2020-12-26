1ee306957af4d8e3ec41e08d29e06e210b4bbc9a
Andrey Kharebov <andreykharebov@gmail.com>
2020-12-27 00:13:15 +0300

    Added rightStatue to tour page

--- src/components/MainSection/TourPage/TourPage.js
+++ src/components/MainSection/TourPage/TourPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classes from './TourPage.module.css';
 import ScriptTag from 'react-script-tag';
+import rigthStatue from '../../../assets/right-statue.png';
 
 function TourPage() {
 
@@ -16,6 +17,9 @@ function TourPage() {
           data-locale='en'
         >Caravan Palace tour dates</a>
         <ScriptTag type='text/javascript' src='//widget.songkick.com/3799121/widget.js' />
+      </div>
+      <div className={ classes.rigthStatue }>
+        <img src={ rigthStatue } alt='rightStatue'/>
       </div>
     </div>
   )
